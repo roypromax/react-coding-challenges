3863abd24159b01b4c0f23772a36aaa45a176b42
roypromax <roy19rishabh@gmail.com>
2023-09-08 05:00:59 +0530

    TodoApp component functionality completed

--- todo-list/src/components/TodoApp.jsx
+++ todo-list/src/components/TodoApp.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+import TodoList from "./TodoList";
 
 const TodoApp = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    setTasks((oldTasks) => {
+      return [...oldTasks, newTask];
+    });
+  };
+
+  const removeTask = (index) => {
+    setTasks((oldTasks) => {
+      return oldTasks.filter((item, i) => {
+        return i !== index;
+      });
+    });
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
+      <div className="todoInput">
+        <TodoItem addTask={addTask} />
+      </div>
+      <div className="todoList">
+        <TodoList tasks={tasks} removeTask={removeTask} />
+      </div>
     </div>
   );
 };
