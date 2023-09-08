177042308b7aa61387cb50151d4061c2f4f13376
roypromax <roy19rishabh@gmail.com>
2023-09-08 12:46:20 +0530

    Static crypto data created

---
+++ crypto-shop/src/data/cryptoData.js
@@ -0,0 +1,19 @@
+const cryptoData = [
+    {
+        id:1,
+        name:"Bitcoin",
+        price:40000
+    },
+    {
+        id:2,
+        name:"Ethereum",
+        price:2800
+    },
+    {
+        id:3,
+        name:"Litecoin",
+        price:150
+    }
+];
+
+export default cryptoData;
