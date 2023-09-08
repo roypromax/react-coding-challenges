9de9923f125be2b6647760919975ca625d98c5fd
roypromax <roy19rishabh@gmail.com>
2023-09-08 12:36:07 +0530

    Crypto Shop initial setup

---
+++ crypto-shop/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CryptoShopApp from "./components/CryptoShopApp";
+
+const App = () => {
+  return (
+    <div>
+      <CryptoShopApp />
+    </div>
+  );
+};
+
+export default App;
---
+++ crypto-shop/src/components/CryptoShopApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CryptoShopApp = () => {
+  return (
+    <div>
+      <h1>Purchase Crypto</h1>
+    </div>
+  );
+};
+
+export default CryptoShopApp;
